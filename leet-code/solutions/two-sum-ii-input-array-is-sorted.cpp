d8f0ae4535554f905aeb1ac5d5c2a8aa1ad2b8d5
AmiltonCabral <amilton.cabral@splab.ufcg.edu.br>
2024-04-22 08:02:05 -0300

    solution

---
+++ leet-code/solutions/two-sum-ii-input-array-is-sorted.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+   public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        vector<int> ans(2);
+        for (int i = 0; i < numbers.size(); i++) {
+            int requestedAmount = target - numbers[i];
+            if (binary_search(numbers.begin() + i, numbers.end(),
+                              requestedAmount)) {
+                ans[0] = i + 1;
+                ans[1] = lower_bound(numbers.begin() + i + 1, numbers.end(),
+                                     requestedAmount) -
+                         numbers.begin() + 1;
+                return ans;
+            }
+        }
+        return ans;
+    }
+};
