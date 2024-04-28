bb7b149ec2b5221ca938bf0db45948fe93cd22ec
AmiltonCabral <amilton.cabral@splab.ufcg.edu.br>
2024-04-28 18:17:50 -0300

    solution

---
+++ leet-code/solutions/subtree-of-another-tree.cpp
@@ -0,0 +1,28 @@
+//  Definition for a binary tree node.
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode* left, TreeNode* right)
+        : val(x), left(left), right(right) {}
+};
+
+class Solution {
+   public:
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        if (!p && !q) return true;
+        if (!p || !q) return false;
+        return p->val == q->val && isSameTree(p->left, q->left) &&
+               isSameTree(p->right, q->right);
+    }
+
+    bool isSubtree(TreeNode* root, TreeNode* subRoot) {
+        if (root->val == subRoot->val && isSameTree(root, subRoot)) {
+            return true;
+        }
+        return (root->left && isSubtree(root->left, subRoot)) ||
+               (root->right && isSubtree(root->right, subRoot));
+    }
+};
