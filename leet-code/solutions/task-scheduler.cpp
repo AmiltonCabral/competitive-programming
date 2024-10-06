893882f3d3b2da5fdec91a92677b427aee1a437d
AmiltonCabral <amilton.cabral@splab.ufcg.edu.br>
2024-10-06 10:48:23 -0300

    solution

--- leet-code/solutions/task-scheduler.cpp
+++ leet-code/solutions/task-scheduler.cpp
@@ -2,48 +2,55 @@
 using namespace std;
 
 class Solution {
+   private:
+    vector<int> getNextTaskHeap(vector<char>& tasks) {
+        unordered_map<char, int> numTask;
+
+        for (char task : tasks) {
+            ++numTask[task];
+        }
+
+        vector<int> nextTasks;
+        for (auto it = numTask.begin(); it != numTask.end(); ++it) {
+            nextTasks.push_back(it->second);
+        }
+
+        make_heap(nextTasks.begin(), nextTasks.end());
+
+        return nextTasks;
+    }
+
    public:
-    // Time: O(n log 26) -> O(n)
+    // Time: O(n * m log 26 ) -> O(n), m = tasks size
     // Space: O(n)
     int leastInterval(vector<char>& tasks, int n) {
         // min first priority queue
         priority_queue<pair<int, int>, vector<pair<int, int>>,
                        greater<pair<int, int>>>
             pq;
-        vector<int> nextTask;
 
-        // create max Heap
-        unordered_map<char, int> numTask;
-        for (char task : tasks) {
-            ++numTask[task];
-        }
-        for (auto it = numTask.begin(); it != numTask.end(); ++it) {
-            nextTask.push_back(it->second);
-        }
-        numTask.clear();
-        make_heap(nextTask.begin(), nextTask.end());
+        vector<int> nextTasks = getNextTaskHeap(tasks);
 
-        int ans = 0;
-        int time = 0;
-        while (!nextTask.empty() || !pq.empty()) {
-            if (!nextTask.empty()) {
-                int currTask = nextTask[0] - 1;
-                int nextTime = time + n + 1;
+        int ans = 0, time = 0;
+        while (!nextTasks.empty() || !pq.empty()) {
+            ++time;
+            ++ans;
 
-                pop_heap(nextTask.begin(), nextTask.end());
-                nextTask.pop_back();
+            if (!nextTasks.empty()) {
+                int currTask = nextTasks[0] - 1;
+                int nextTime = time + n;
+
+                pop_heap(nextTasks.begin(), nextTasks.end());
+                nextTasks.pop_back();
 
                 if (currTask > 0) {
                     pq.push({nextTime, currTask});
                 }
             }
 
-            ++time;
-            ++ans;
-
             while (!pq.empty() && pq.top().first <= time) {
-                nextTask.push_back(pq.top().second);
-                push_heap(nextTask.begin(), nextTask.end());
+                nextTasks.push_back(pq.top().second);
+                push_heap(nextTasks.begin(), nextTasks.end());
 
                 pq.pop();
             }
