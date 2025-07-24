40d1e54ba11b707c20663f8bd6fdfae888cae8ae
Amilton Cabral <amilton.cabral@vtex.com>
2025-07-24 20:37:24 -0300

    sol

---
+++ leet-code/solutions/sqrtx.cpp
@@ -0,0 +1,35 @@
+#include <functional>
+using namespace std;
+
+class Solution {
+   public:
+    int mySqrt1(int x) {
+        long long prev = 0;
+
+        for (long long i = 0; i * i <= x; ++i) {
+            if (i * i == x) return i;
+            prev = i;
+        }
+
+        return prev;
+    }
+
+    int mySqrt(int x) {
+        int ans;
+        int l = 0;
+        int r = x;
+
+        while (l <= r) {
+            long long mid = l + (r - l) / 2;
+            if (mid * mid == x) return mid;
+            if (mid * mid < x) {
+                l = mid + 1;
+                ans = mid;
+            } else {
+                r = mid - 1;
+            }
+        }
+
+        return ans;
+    }
+};
